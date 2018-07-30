c95bb90227e9e245e701f4076d87cbdc4b003aab
Scott Frechette <scottfrechette@gmail.com>
2018-07-29 22:45:22 -0500

    Complete Homework 1 assignment

--- homework_1_assignment.R
+++ homework_1_assignment.R
@@ -27,30 +27,60 @@ productivity_cost_by_role_tbl
 
 # Q1: Which Job Role has the highest total cost of attrition? ----
 
-dept_jobrole_tbl %>% 
+dept_jobrole_productivity_tbl <- dept_jobrole_tbl %>% 
   
   count(Department, JobRole, Attrition) %>%
   count_to_pct(Department, JobRole) %>%
-  assess_attrition(Attrition, attrition_value = "Yes", baseline_pct = 0.088) %>% 
-  left_join(productivity_cost_by_role_tbl) %>% 
-  mutate(cost_of_attrition = calculate_attrition_cost(n = n, salary = Salary_Average)) %>% 
-  arrange(-cost_of_attrition) %>% 
-  select(Department, JobRole, cost_of_attrition)
+  assess_attrition(Attrition, attrition_value = "Yes", 
+                   baseline_pct = 0.088) %>% 
+  
+  left_join(productivity_cost_by_role_tbl, 
+            by = c("Department", "JobRole")) %>% 
+  
+  mutate(cost_of_attrition = calculate_attrition_cost(n = n, 
+                                                      salary = Salary_Average,
+                                                      net_revenue_per_employee = Revenue_Average)) %>% 
+  arrange(-cost_of_attrition)
 
-# Laboratory Tech
+dept_jobrole_productivity_tbl %>% 
+  select(Department, JobRole, cost_of_attrition) %>% 
+  slice(1)
+
+# Sales executive
 
 # Q2: What is the total cost of attrition for the Research & Development: Research Scientist job role? ----
 
-# $3.27M
+dept_jobrole_productivity_tbl %>% 
+  filter(JobRole == "Research Scientist") %>% 
+  select(Department, JobRole, cost_of_attrition)
+
+# $2.28M
 
 # Q3: What percentage do the top four Job Roles account for in terms of the total cost of attrition? ----
 
-#82.9%
+dept_jobrole_productivity_tbl %>% 
+  mutate(cumulative_attrition = cumsum(cost_of_attrition),
+         pct = cumulative_attrition/sum(cost_of_attrition)) %>% 
+  slice(1:4) %>% 
+  select(Department, JobRole, cost_of_attrition, cumulative_attrition, pct)
+
+#86.1%
 
 # Q4. Which Department has the highest total cost of attrition? ----
 
-# R& D
+dept_jobrole_productivity_tbl %>% 
+  group_by(Department) %>%
+  summarise(dept_cost_of_attrition = sum(cost_of_attrition)) %>% 
+  arrange(-dept_cost_of_attrition)
+
+# Sales
 
 # Q5: What percentage does the top Department account for in terms of the total cost of attrition? ----
-# 57%
 
+dept_jobrole_productivity_tbl %>% 
+  group_by(Department) %>%
+  summarise(dept_cost_of_attrition = sum(cost_of_attrition)) %>%
+  arrange(-dept_cost_of_attrition) %>% 
+  mutate(pct = dept_cost_of_attrition / sum(dept_cost_of_attrition))
+
+# 50.1%
